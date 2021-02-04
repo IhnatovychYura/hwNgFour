36a9417d1db519f713360d59f6c96386f5b281eb
IhnatovychYura <yuraihnatovych111@gmail.com>
2021-02-04 20:30:28 +0200

    Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './components/users/users.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UserResolveService } from './services/resolve/user-resolve.service';
+import { UserComponent } from './components/user/user.component';
+import { FullUserComponent } from './components/full-user/full-user.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'users', pathMatch: 'full'},
+  {path: 'users', component: UsersComponent, resolve: {usersData: UserResolveService},
+  children: [
+    {path: ':id', component: FullUserComponent}
+  ]},
+  ];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    FullUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
